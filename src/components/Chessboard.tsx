7bda4cee380912f976d1ff9b2df24e34779d8651
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-05-10 22:38:14 +0530

    for loop

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
-import './Chessboard.css';
+import "./Chessboard.css";
 
-const verticalAxis = [1,2,3,4,5,6,7,8];
+const verticalAxis = ["1","2","3","4","5","6","7","8"];
 const horizontalAxis = ["a","b","c","d","e","f","g","h"];
 
 export default function Chessboard(){
     let board = [];
 
-    for(let i = 0;i<horizontalAxis.length;i++){
-        for(let j = 0;j<verticalAxis.length;j++){
-            board.push(<span> {horizontalAxis[i]} {verticalAxis[j]} </span>)
+    for(let j = verticalAxis.length - 1;j>=0;j--){
+        for(let i = 0;i<horizontalAxis.length;i++){
+            board.push(
+                <span>{horizontalAxis[i]} {verticalAxis[j]}</span>);
         }
     }
 return <div id="chessboard">{board}</div>
