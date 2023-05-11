997020ffabd088069474c5f00e7266fdb388baf5
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-05-11 10:55:10 +0530

    chess board commpleation

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -10,8 +10,15 @@ export default function Chessboard(){
 
     for(let j = verticalAxis.length - 1;j>=0;j--){
         for(let i = 0;i<horizontalAxis.length;i++){
+            const number = j+i+2;
+            if(number % 2 === 0){
+                board.push(
+                    <div className="tile black-tile"></div>
+                );
+            } else{
             board.push(
-                <div className="tile">{horizontalAxis[i]} {verticalAxis[j]}</div>);
+                <div className="tile white-tile"></div>);
+            }
         }
     }
 return <div id="chessboard">{board}</div>
