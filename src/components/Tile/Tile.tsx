4924f0714809bc35f050df9d5ddb017d3f885db7
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-05-12 12:05:20 +0530

    center the pawn content

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 export default function Tile({ number }: Props){
     if(number % 2 === 0){
-        return <div className='tile black-tile'><img src='assets/images/pawn_b.png'/></div>;
+        return <div className='tile black-tile'><img src='assets/images/pawn_b.png' alt='pawn'/></div>;
     } else{
         return <div className='tile white-tile'></div>;
     }
