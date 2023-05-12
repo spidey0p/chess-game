107c291a8179c27415bf5ca9ae80fe217d5e5a4f
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-05-12 11:35:36 +0530

    done with inserting pawn

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,13 +1,13 @@
-import '/Tile.css';
+import './Tile.css';
 
 interface Props {
     number: number;
 }
 export default function Tile({ number }: Props){
     if(number % 2 === 0){
-        return <div className='tile black-tile'>hello</div>
+        return <div className='tile black-tile'><img src='assets/images/pawn_b.png'/></div>;
     } else{
-        return <div className='tile white-tile'>hii</div>
+        return <div className='tile white-tile'></div>;
     }
 }
 
