1ad3dc0d1f19fc0808d5872efcfc9f1ea6ef97fe
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-05-13 12:37:39 +0530

    completion of indexing insert of element

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -5,20 +5,31 @@ import "./Chessboard.css";
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
+interface Piece {
+  image: string;
+  x: number;
+  y: number;
+}
+
+const pieces: Piece[] = [];
+
+pieces.push({image: "assets/images/pawn_b.png",x:0,y:1})
+
 export default function Chessboard() {
   let board = [];
 
   for (let j = verticalAxis.length - 1; j >= 0; j--) {
     for (let i = 0; i < horizontalAxis.length; i++) {
         const number = j + i + 2;
+        let image = undefined;
+        pieces.forEach(p => {
+          if(p.x === i && p.y === j){
+            image = p.image;
+          } 
+        })
 
-        board.push(<Tile number={number} />);
+        board.push(<Tile image={image} number={number} />);
 
-        // if(number % 2 === 0) {
-        //     board.push(<Tile />);
-        // } else {
-        //     board.push(<Tile />);
-        // }
     }
   }
 
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,13 +1,22 @@
 import './Tile.css';
 
 interface Props {
+    image?: string;
     number: number;
 }
-export default function Tile({ number }: Props){
+export default function Tile({ number,image }: Props){
     if(number % 2 === 0){
-        return <div className='tile black-tile'><img src='assets/images/pawn_b.png' alt='pawn'/></div>;
+        return( <div className='tile black-tile'>
+            <img src={image} alt='' />
+        </div>
+        );
     } else{
-        return <div className='tile white-tile'></div>;
+        return (
+            <div className='tile white-tile'>
+                {" "}
+                <img src={image} alt='' />
+         </div>
+         );
     }
 }
 
