dd1b5fc768d1876eed5530750e2d3a92163312e0
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-05-13 13:29:24 +0530

    Placed all pices respect to those positions

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -12,8 +12,48 @@ interface Piece {
 }
 
 const pieces: Piece[] = [];
+//for black pawn.
+for(let i = 0; i<8; i++){
+pieces.push({image: "assets/images/pawn_b.png",x:i,y:6})
+}
+//Rooks
+pieces.push({image: "assets/images/rook_b.png",x:0,y:7});
+pieces.push({image: "assets/images/rook_b.png",x:7,y:7});
+//Bishops
+pieces.push({image: "assets/images/bishop_b.png",x:2,y:7});
+pieces.push({image: "assets/images/bishop_b.png",x:5,y:7});
+//Knight
+pieces.push({image: "assets/images/knight_b.png",x:1,y:7});
+pieces.push({image: "assets/images/knight_b.png",x:6,y:7});
 
-pieces.push({image: "assets/images/pawn_b.png",x:0,y:1})
+//Queen piece
+pieces.push({image: "assets/images/queen_b.png",x:3,y:7});
+//King piece
+pieces.push({image: "assets/images/king_b.png",x:4,y:7});
+
+
+//for white pawn.
+for(let i = 0;i<8;i++){
+  pieces.push({image: "assets/images/pawn_w.png",x:i,y:1});
+}
+
+//Rooks
+pieces.push({image: "assets/images/rook_w.png",x:0,y:0});
+pieces.push({image: "assets/images/rook_w.png",x:7,y:0});
+//Bishops
+pieces.push({image: "assets/images/bishop_w.png",x:2,y:0});
+pieces.push({image: "assets/images/bishop_w.png",x:5,y:0});
+//Knight
+pieces.push({image: "assets/images/knight_w.png",x:1,y:0});
+pieces.push({image: "assets/images/knight_w.png",x:6,y:0});
+
+//Queen piece
+pieces.push({image: "assets/images/queen_w.png",x:3,y:0});
+//King piece
+pieces.push({image: "assets/images/king_w.png",x:4,y:0});
+
+
+
 
 export default function Chessboard() {
   let board = [];
